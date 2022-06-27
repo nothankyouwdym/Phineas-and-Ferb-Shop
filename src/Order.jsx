ac46c4a65d557f532186c27aaed662f4383f4147
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-27 10:23:59 -0400

    1 file modified, 1 added

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+import OrderTable from "./OrderTable"
+
 const Order = () =>{
+
+  const [item,setItem] = useState([]) 
+
+  const handleChange = (e) =>{
+    setItem([...item,e.target.value])
+  }
   return(
     <section>
       <h1>Order Page</h1>
+      <select multiple onClick={handleChange}>
+        <option value="Hat">Hat</option>
+        <option value="T-Shirt">T-Shirt</option>
+        <option value="Dog Food">Dog Food</option>
+        <option value="Cat Food">Cat Food</option>
+      </select>
+
+      <OrderTable props={item}/>
     </section>
   )
 }
---
+++ src/OrderTable.jsx
@@ -0,0 +1,22 @@
+
+
+
+const OrderTable = () =>{
+  return(
+    <section>
+      <table>
+        <THead />
+      </table>
+    </section>
+  )
+}
+
+const THead = () =>{
+  return(
+    <thead>
+      <tr>
+        <th>Item</th>
+      </tr>
+    </thead>
+  )
+}
