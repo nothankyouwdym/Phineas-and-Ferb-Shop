7bb27e39db18573961208e69970648396bd038ba
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-27 12:25:55 -0400

    1 file modified

--- src/OrderTable.jsx
+++ src/OrderTable.jsx
@@ -1,7 +1,11 @@
-const OrderTable = () =>{
+import data from "./Data/data"
+
+
+const OrderTable = (props) =>{
   return(
     <table>
-
+      <THead />
+      <TBody props={props}/>
     </table>
   )
 }
@@ -11,9 +15,32 @@ const THead = () =>{
     <thead>
       <tr>
         <th>Item</th>
+        <th>Price</th>
       </tr>
     </thead>
   )
+}
+
+const TBody = (props) =>{
+  const rows = props.props.props.item.map((row,index) =>{
+    return(
+      <tr key={index}>
+        <th>{row}</th>
+        <th>{getPrices(row)}</th>
+      </tr>
+    )
+  })
+
+  return <tbody>{rows}</tbody>
+}
+
+
+const getPrices = (row) =>{
+  for(let i = 0; i < data.length; i++){
+      if(data[i].name === row){
+        return data[i].price
+      }
+  }
 }
 
 
